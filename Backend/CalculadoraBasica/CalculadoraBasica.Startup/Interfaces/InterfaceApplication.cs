c31ae97db25e6f8536fd16322ae7fdd5f97bad9b
Juliet <julietcaro40@gmail.com>
2026-03-19 01:24:11 -0500

    Calculadora finalizada

---
+++ Backend/CalculadoraBasica/CalculadoraBasica.Startup/Interfaces/InterfaceApplication.cs
@@ -0,0 +1,10 @@
+﻿namespace CalculadoraBasica.Startup.Interfaces
+{
+    public interface InterfaceApplication
+    {
+        void Start();
+        void ShowMessage(string message, ConsoleColor color = ConsoleColor.White);
+        string ShowQuestion(string question, ConsoleColor color = ConsoleColor.White);
+        void ShowHelp();
+    }
+}
---
+++ Backend/CalculadoraBasica/CalculadoraBasica.Startup/Program.cs
@@ -0,0 +1,5 @@
+﻿using CalculadoraBasica.Startup.Classes;
+
+var app = new Application("CalculadoraBasica", "v0.1.0");
+
+app.Start();
