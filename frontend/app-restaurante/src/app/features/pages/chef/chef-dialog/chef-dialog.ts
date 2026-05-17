7a406e3884b7d1f9ed3a55515ac7c2d4c88e4fc7
Juliet <julietcaro40@gmail.com>
2026-05-17 15:36:20 -0500

    feat: implementación del dialog (eliminar) de chef

--- frontend/app-restaurante/src/app/features/pages/chef/chef-dialog/chef-dialog.ts
+++ frontend/app-restaurante/src/app/features/pages/chef/chef-dialog/chef-dialog.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { IChef } from '../../../interfaces/chefs';
+import { ChefService } from '../../../services/chef-service';
+
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { GenericDialog } from '../../../../shared/components/generic-dialog/generic-dialog';
 
 @Component({
   selector: 'app-chef-dialog',
@@ -6,4 +12,49 @@ import { Component } from '@angular/core';
   templateUrl: './chef-dialog.html',
   styleUrl: './chef-dialog.scss',
 })
-export class ChefDialog {}
+export class ChefDialog {
+  private _chefService = inject(ChefService);
+  private snackBar = inject(MatSnackBar);
+  private dialogData = inject(MAT_DIALOG_DATA);
+
+  chef: IChef | null = null;
+  loading = signal(false);
+
+  ngOnInit() {
+    if (this.dialogData) {
+      this.chef = this.dialogData.chef;
+      this.dialogData.onSave?.subscribe(() => this.eliminar());
+    }
+  }
+
+  eliminar() {
+    if (!this.chef) return;
+    this.loading.set(true);
+
+    this._chefService.deleteChef(this.chef.id).subscribe({
+      next: () => {
+        this.loading.set(false);
+        this.snackBar.open(
+          `Chef "${this.chef?.name}" eliminada correctamente`,
+          'Cerrar',
+          { duration: 2000 }
+        );
+
+        this.dialogData.dialogRef?.close(true); //cerrar el dialog y actualizar 
+      },
+
+      error: () => {
+        this.loading.set(false);
+        this.snackBar.open(
+          'Error al eliminar al chef',
+          'Cerrar',
+          { duration: 2000 }
+        );
+      }
+    });
+  }
+
+  cancelar() {
+    this.dialogData.dialogRef.close(false);
+  }
+}
